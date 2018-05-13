a0f277e7ffcc0e29fbe3a437c7ee6a4eb8986cae
Anders Busch <andersbusch@gmail.com>
2018-05-13 23:06:43 +0200

    vertical scrolling!!

--- kilotxt.c
+++ kilotxt.c
@@ -35,6 +35,7 @@ struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;
     int screenrows;
     int screencols;
     int numrows;
@@ -229,15 +230,26 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+void editorScroll() {
+    if ( E.cy < E.rowoff ) {
+        E.rowoff = E.cy;
+    }
+    if ( E.cy >= E.rowoff + E.screenrows ) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
     int y;
     int rows = E.screenrows;
     int cols = E.screencols;
     for ( y = 0; y < rows; ++y ) {
-        if ( y >= E.numrows ) {
+        int filerow = y + E.rowoff;
+        if ( filerow >= E.numrows ) {
             if ( E.numrows == 0 && y == rows / 3 ) {
                 char welcome[80];
-                int welcomelen = snprintf(welcome, sizeof(welcome), "Kilotxt editor -- version %s", KILOTXT_VERSION);
+                int welcomelen = snprintf(welcome, sizeof(welcome),
+                        "Kilotxt editor -- version %s", KILOTXT_VERSION);
                 if ( welcomelen > cols ) {
                     welcomelen = cols;
                 } 
@@ -246,16 +258,16 @@ void editorDrawRows(struct abuf *ab) {
                     abAppend(ab, "~", 1);
                     padding--;
                 }
-                while ( padding-- ) abAppend(ab, " ", 1);
+                while (padding--) abAppend(ab, " ", 1);
                 abAppend(ab, welcome, welcomelen);
             } else {
                 abAppend(ab, "~", 1);
             }
 
         } else {
-            int len = E.row[y].size;
-            if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            int len = E.row[filerow].size;
+            if ( len > cols ) len = cols;
+            abAppend(ab, E.row[filerow].chars, len);
         }
         abAppend(ab, "\x1b[K", 3);
         if ( y < rows - 1 ) {
@@ -265,6 +277,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
     abAppend(&ab, "\x1b[?25l", 6); /* hide cursor (newer mode might only work newer VT-models) */
     abAppend(&ab, "\x1b[H", 3); /* return cursor to start of screen */
@@ -273,7 +287,7 @@ void editorRefreshScreen() {
 
     /* setting cursor position according to struct */
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
     
     abAppend(&ab, "\x1b[?25h", 6); /* show cursor again */
@@ -302,7 +316,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_DOWN:
-            if ( E.cy != E.screenrows - 1 ) {
+            if ( E.cy < E.numrows ) {
                 E.cy++;
             }
             break;
@@ -349,6 +363,7 @@ void initEditor() {
     E.cx = 0; /* cursor location */
     E.cy = 0;
     E.numrows = 0;
+    E.rowoff = 0;
     E.row = NULL;
     if ( getWindowSize(&E.screenrows, &E.screencols) == -1 ) {
         die("getWindowSize failed");
